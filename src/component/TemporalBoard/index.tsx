6246389214ed26d1db04d0166a4a82798458730c
jongseokleedev <jongsuklee45@gmail.com>
2023-03-25 18:53:01 +0900

    implemented holyGenerator frame

--- src/component/TemporalBoard/index.tsx
+++ src/component/TemporalBoard/index.tsx
@@ -7,6 +7,8 @@ import text_img from "./text_img.svg";
 import font_img from "./font_img.svg";
 import background_img from "./background_img.svg";
 import sticker_img from "./sticker_img.svg";
+import info_img from "./info.svg";
+import share_img from "./share.svg";
 interface Props extends Properties {
   randomText: Function;
 }
@@ -16,29 +18,39 @@ export default function TemporalBoard(properties: Props) {
 
   return (
     <div id={id} className={cl}>
-      <div className="BottomStruct">
-        <div className="TextButton" onClick={() => properties.randomText()}>
-          <img src={text_img} className="ButtonImg" />
+      <div className="ButtomContainer">
+        <div className="ButtonContainer">
+          <div className="BottomStruct">
+            <div className="ButtonText">문구</div>
+            <div className="TextButton" onClick={() => properties.randomText()}>
+              <img src={text_img} className="ButtonImg" />
+            </div>
+          </div>
+          <div className="BottomStruct">
+            <div className="ButtonText">폰트</div>
+            <div className="FontButton">
+              <img src={font_img} className="ButtonImg" />
+            </div>
+          </div>
+          <div className="BottomStruct">
+            <div className="ButtonText">배경</div>
+            <div className="BackgroundButton" onClick={() => tapestry.choose()}>
+              <img src={background_img} className="ButtonImg" />
+            </div>
+          </div>
+          <div className="BottomStruct">
+            <div className="ButtonText">스티커</div>
+            <div className="StickerButton" onClick={() => hanger.create("")}>
+              <img src={sticker_img} className="ButtonImg" />
+            </div>
+          </div>
         </div>
-        <div className="ButtonText">문구</div>
-      </div>
-      <div className="BottomStruct">
-        <div className="FontButton">
-          <img src={font_img} className="ButtonImg" />
+        <div></div>
+        <div className="FooterContainer" onClick={() => hanger.create("")}>
+          <img src={info_img} className="ButtonImg" />
+          <div className="ButtonText">오늘의 말씀</div>
+          <img src={share_img} className="ButtonImg" />
         </div>
-        <div className="ButtonText">폰트</div>
-      </div>
-      <div className="BottomStruct">
-        <div className="BackgroundButton" onClick={() => tapestry.choose()}>
-          <img src={background_img} className="ButtonImg" />
-        </div>
-        <div className="ButtonText">배경</div>
-      </div>
-      <div className="BottomStruct">
-        <div className="StickerButton" onClick={() => hanger.create("")}>
-          <img src={sticker_img} className="ButtonImg" />
-        </div>
-        <div className="ButtonText">스티커</div>
       </div>
     </div>
   );
